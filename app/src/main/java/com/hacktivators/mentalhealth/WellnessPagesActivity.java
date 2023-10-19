0a47e6153c03151640d020ecb45d34d576673959
nazzmeen <nazmeensultana666@gmail.com>
2023-10-20 01:37:57 +0530

    wellnesspagedone

--- app/src/main/java/com/hacktivators/mentalhealth/WellnessPagesActivity.java
+++ app/src/main/java/com/hacktivators/mentalhealth/WellnessPagesActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class meditationpage extends AppCompatActivity {
+public class WellnessPagesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meditationpage);
+        setContentView(R.layout.activity_wellnesspage);
     }
 }
