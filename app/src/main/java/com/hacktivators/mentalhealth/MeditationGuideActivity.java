1b6f5e4630e823facf42e2f54f7a072c9f8c48b6
nazzmeen <nazmeensultana666@gmail.com>
2023-10-20 21:18:47 +0530

    wellnessblock/maeditationguide

--- app/src/main/java/com/hacktivators/mentalhealth/MeditationGuideActivity.java
+++ app/src/main/java/com/hacktivators/mentalhealth/MeditationGuideActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class WellnessBlock extends AppCompatActivity {
+public class MeditationGuideActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wellness_block);
+        setContentView(R.layout.activity_meditation_guide);
     }
 }
