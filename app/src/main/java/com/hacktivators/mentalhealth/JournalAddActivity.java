5b86cd4596fddeaf6cda4fed1409969225a4b98c
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2023-10-20 06:15:36 +0530

    feat: journal finish and minor UI improvements

--- app/src/main/java/com/hacktivators/mentalhealth/JournalAddActivity.java
+++ app/src/main/java/com/hacktivators/mentalhealth/JournalAddActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.cardview.widget.CardView;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,45 +24,86 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
 
 public class JournalAddActivity extends AppCompatActivity {
 
 
-    AppCompatButton refreshQuestion,close,save,newQuestion;
+    AppCompatButton close,save;
 
     TextView generatedQuestion;
 
     EditText journal_box;
 
     boolean QuestionBol = false;
+    boolean color = false;
 
     FirebaseUser firebaseUser;
 
     String JournalID;
 
-    String Question;
+    String Question = "Random Question";
 
     LinearLayout colors;
 
+
+
     CardView red,blue,green,purple,yellow;
 
+    String ColorSelected = "null";
+    ImageView redCheck,blueCheck,greenCheck,purpleCheck,yellowCheck;
+
+    ImageView refreshQuestion,cancelQuestion;
+
+    private Random randomGenerator;
+    ArrayList<String> Questions = new ArrayList<>();
+
+    ImageButton newQuestion;
+    RelativeLayout generateQuesLayout;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        randomGenerator = new Random();
+
+        Questions.add("What's been on your mind lately?");
+        Questions.add("What's been bothering you?");
+        Questions.add("What are you grateful for today?");
+        Questions.add("What are you grateful for in your life?");
+        Questions.add("Describe a recent experience that had an impact on you.");
+        Questions.add("What are your short-term goals?");
+        Questions.add("What are your long-term goals?");
+        Questions.add("Reflect on something you learned recently");
+        Questions.add("What's something you'd like to change in your life?");
+        Questions.add("Share your favorite quote and its significance");
+
+
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_add);
 
 
-        refreshQuestion = findViewById(R.id.refresh);
+
 
         close = findViewById(R.id.close);
 
         save = findViewById(R.id.save);
 
-        newQuestion = findViewById(R.id.newQuestion);
+        newQuestion = findViewById(R.id.add_icon);
+        refreshQuestion = findViewById(R.id.refresh_icon);
+        cancelQuestion = findViewById(R.id.remove_icon);
 
         generatedQuestion = findViewById(R.id.generatedQuestion);
+        generateQuesLayout = findViewById(R.id.genQuesLayout);
+
 
         journal_box = findViewById(R.id.journal_box);
 
@@ -65,7 +112,211 @@ public class JournalAddActivity extends AppCompatActivity {
         colors = findViewById(R.id.color);
 
 
+        red = findViewById(R.id.red);
+        blue = findViewById(R.id.blue);
+        green = findViewById(R.id.green);
+        purple = findViewById(R.id.purple);
+        yellow = findViewById(R.id.yellow);
 
+        redCheck = findViewById(R.id.red_check);
+        blueCheck = findViewById(R.id.blue_check);
+        greenCheck = findViewById(R.id.green_check);
+        purpleCheck = findViewById(R.id.purple_check);
+        yellowCheck = findViewById(R.id.yellow_check);
+
+
+
+
+
+        generateQuesLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(cancelQuestion.getVisibility() == View.INVISIBLE){
+                    newQuestion.setVisibility(View.INVISIBLE);
+                    cancelQuestion.setVisibility(View.VISIBLE);
+                    refreshQuestion.setVisibility(View.VISIBLE);
+
+                    Toast.makeText(getApplicationContext(),"New clicked",Toast.LENGTH_SHORT).show();
+                    int index = randomGenerator.nextInt(Questions.size());
+                    String question = Questions.get(index);
+
+
+                    generatedQuestion.setText(question);
+
+                    QuestionBol = true;
+
+                }
+
+
+
+            }
+
+
+        });
+
+        refreshQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                refreshQuestionFunc();
+
+
+            }
+        });
+
+        cancelQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Cancel clicked",Toast.LENGTH_SHORT).show();
+                newQuestion.setVisibility(View.VISIBLE);
+                cancelQuestion.setVisibility(View.INVISIBLE);
+                refreshQuestion.setVisibility(View.INVISIBLE);
+
+                generatedQuestion.setText("Cant think of any? Add these questions!!");
+
+                QuestionBol = false;
+            }
+
+        });
+
+
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ColorSelected.equals("red")) {
+                    redCheck.setVisibility(View.GONE);
+                    color = false;
+                    ColorSelected = "null";
+                }else {
+                    redCheck.setVisibility(View.VISIBLE);
+                    blueCheck.setVisibility(View.GONE);
+                    greenCheck.setVisibility(View.GONE);
+                    purpleCheck.setVisibility(View.GONE);
+                    yellowCheck.setVisibility(View.GONE);
+
+
+
+                    color = true;
+                    ColorSelected = "red";
+                }
+
+
+
+
+
+
+            }
+        });
+
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(ColorSelected.equals("blue")) {
+                    blueCheck.setVisibility(View.GONE);
+                    color = false;
+                    ColorSelected = "null";
+                }else {
+                    redCheck.setVisibility(View.GONE);
+                    blueCheck.setVisibility(View.VISIBLE);
+                    greenCheck.setVisibility(View.GONE);
+                    purpleCheck.setVisibility(View.GONE);
+                    yellowCheck.setVisibility(View.GONE);
+
+
+
+                    color = true;
+                    ColorSelected = "blue";
+
+                }
+
+
+
+
+
+
+            }
+        });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                if(ColorSelected.equals("green")) {
+                    greenCheck.setVisibility(View.GONE);
+                    color = false;
+                    ColorSelected = "null";
+                }else {
+                    redCheck.setVisibility(View.GONE);
+                    blueCheck.setVisibility(View.GONE);
+                    greenCheck.setVisibility(View.VISIBLE);
+                    purpleCheck.setVisibility(View.GONE);
+                    yellowCheck.setVisibility(View.GONE);
+
+
+
+                    color = true;
+                    ColorSelected = "green";
+                }
+
+
+
+
+
+            }
+        });
+
+        purple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ColorSelected.equals("purple")) {
+                    purpleCheck.setVisibility(View.GONE);
+                    color = false;
+                    ColorSelected = "null";
+                }else {
+                    redCheck.setVisibility(View.GONE);
+                    blueCheck.setVisibility(View.GONE);
+                    greenCheck.setVisibility(View.GONE);
+                    purpleCheck.setVisibility(View.VISIBLE);
+                    yellowCheck.setVisibility(View.GONE);
+
+
+
+                    color = true;
+                    ColorSelected = "purple";
+                }
+
+
+
+
+
+
+            }
+        });
+
+        yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ColorSelected.equals("yellow")) {
+                    yellowCheck.setVisibility(View.GONE);
+                    color = false;
+                    ColorSelected = "null";
+                }else {
+                    redCheck.setVisibility(View.GONE);
+                    blueCheck.setVisibility(View.GONE);
+                    greenCheck.setVisibility(View.GONE);
+                    purpleCheck.setVisibility(View.GONE);
+                    yellowCheck.setVisibility(View.VISIBLE);
+
+                    color = true;
+                    ColorSelected = "yellow";
+
+                }
+
+            }
+        });
 
         newQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,6 +335,13 @@ public class JournalAddActivity extends AppCompatActivity {
             }
         });
 
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
 
 
@@ -91,7 +349,25 @@ public class JournalAddActivity extends AppCompatActivity {
 
     }
 
+    private void refreshQuestionFunc() {
+
+        Toast.makeText(getApplicationContext(), "Refresh clicked", Toast.LENGTH_SHORT).show();
+        int index = randomGenerator.nextInt(Questions.size());
+        String question = Questions.get(index);
+        if (generatedQuestion.getText().toString().equals(Question)){
+            refreshQuestionFunc();
+        }else {
+            generatedQuestion.setText(question);
+        }
+
+    }
+
     private void saveJournal(String Journal) {
+
+        Date date = new Date();
+
+        long date_ = date.getTime();
+
 
 
 
@@ -99,17 +375,16 @@ public class JournalAddActivity extends AppCompatActivity {
 
         FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
         HashMap hashMap = new HashMap();
+        hashMap.put("ID",JournalID);
+        hashMap.put("journal",Journal);
+        hashMap.put("date",date_);
         if(QuestionBol){
-
-            hashMap.put("ID",JournalID);
-            hashMap.put("journal",Journal);
             hashMap.put("question",true);
             hashMap.put("questionTxt",Question);
-        }else {
-
-            hashMap.put("ID",JournalID);
-            hashMap.put("journal",Journal);
-            hashMap.put("question",false);
+        }
+        if(color){
+            hashMap.put("color",true);
+            hashMap.put("colorTxt",ColorSelected);
         }
 
 
@@ -118,6 +393,7 @@ public class JournalAddActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<Void> task) {
                 if(task.isSuccessful()){
                     Toast.makeText(getApplicationContext(),"Journal saved!",Toast.LENGTH_SHORT).show();
+                    finish();
                 }else {
                     Toast.makeText(getApplicationContext(),"Something went wrong!",Toast.LENGTH_SHORT).show();
                 }
