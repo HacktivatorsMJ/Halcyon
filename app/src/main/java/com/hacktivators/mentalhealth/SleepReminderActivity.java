de37f1983409339440c610aa663f597782cfafdd
nazzmeen <nazmeensultana666@gmail.com>
2023-10-22 11:27:29 +0530

    sleepreminderpage

---
+++ app/src/main/java/com/hacktivators/mentalhealth/SleepReminderActivity.java
@@ -0,0 +1,14 @@
+package com.hacktivators.mentalhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_reminder);
+    }
+}
