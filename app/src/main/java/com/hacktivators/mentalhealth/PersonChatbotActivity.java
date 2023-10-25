e313ba6f2cf41f6feb3f9df528c12a4e8c0c2730
Amenawajiha <amenaw1982@gmail.com>
2023-10-25 23:57:15 +0530

    chatbots ui

---
+++ app/src/main/java/com/hacktivators/mentalhealth/PersonChatbotActivity.java
@@ -0,0 +1,14 @@
+package com.hacktivators.mentalhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonChatbotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_chatbot);
+    }
+}
