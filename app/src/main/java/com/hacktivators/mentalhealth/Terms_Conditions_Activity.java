f6177d4cf9e2a133311f13a4b264b91c1b6bf638
Amenawajiha <amenaw1982@gmail.com>
2023-10-28 01:56:45 +0530

    terms and privacy

---
+++ app/src/main/java/com/hacktivators/mentalhealth/Terms_Conditions_Activity.java
@@ -0,0 +1,14 @@
+package com.hacktivators.mentalhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Terms_Conditions_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_conditions);
+    }
+}
